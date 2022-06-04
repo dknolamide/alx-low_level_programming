cee66d624baa3f6a0feb2edfd4667fefad8b9257
Olamide <dknolamide@gmail.com>
2022-06-04 13:17:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		put char(letter);
+
+	putchar('\n');
+
+	return (0);
+}
