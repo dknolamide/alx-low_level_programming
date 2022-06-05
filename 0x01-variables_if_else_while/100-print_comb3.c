b4d8073e7fc0b6467732136fb61fe35a84bdc83e
Olamide <dknolamide@gmail.com>
2022-06-04 21:50:37 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 0; digit < 9; digit1++)
+	for (digit1 = 0; digit < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digits2 < 10; digits2++)
 		{
