cd42d6b5b72ca1c6070e7ee71b3377c6d480bea7
Olamide <dknolamide@gmail.com>
2022-06-16 07:59:40 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  * @r_index: The current index of the buffer.
  *
  * Return: If r can store the sum - a pointer to the result.
- * 	 If r cannot store the sum - 0.
+ * If r cannot store the sum - 0.
  */
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
@@ -48,6 +48,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	else if (tens && r_index < 0)
 		return (0);
+
 	return (r + r_index + 1);
 }
 /**
@@ -58,4 +59,25 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  * @size_r: The buffer size.
  *
  * Return: If r can store the sum - a pointer to the result.
- *
+ *	If r cannot store the sum - 0.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int index, n1_len = 0, n2_len = 0;
+
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
+}
